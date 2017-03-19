e199410c1280f05de16040dc1b0d81796118ee8a
10BitQubit <stennly.mkiv@gmail.com>
2017-03-19 11:54:47 +0000

    Added blinking effect, with a variable for setting rate of blinking.

--- micro_transmitter_r.py
+++ micro_transmitter_r.py
@@ -2,13 +2,29 @@ from microbit import *
 import radio
 from math import sqrt, atan2, degrees
 
+# Setup for blinking effect on display
+blinker = Image("90909:"
+                "09090:"
+                "90909:"
+                "09090:"
+                "90909")
+blinker_counter = 0
+blink_timer = 70 # 200 changes state about every ~1sec, DECREASE to make it faster
+
 prev = 1000.0
 radio.on()
 while True:
     x = accelerometer.get_x()
     y = accelerometer.get_y()
     z = accelerometer.get_z()
-
+    
+    # Blinking effect on display
+    display.show(blinker)
+    blinker_counter = blinker_counter + 1
+    if(blinker_counter % blink_timer == 0):
+        blinker = blinker.invert()
+    print(blinker_counter)
+    
     curr = sqrt(x**2 + y**2 + z**2)
 
   #  if curr-prev > 1000.0:
@@ -17,23 +33,23 @@ while True:
     if accelerometer.was_gesture("down"):
         # uart.write("D")
         radio.send("RD")
-#        display.show("D");
-        display.show(Image.ARROW_N)
+        # display.show("D");
+        # display.show(Image.ARROW_N)
     if accelerometer.was_gesture("up"):
         # uart.write("U")
         radio.send("RU")
-#        display.show("U");
-        display.show(Image.ARROW_S)
+        # display.show("U");
+        # display.show(Image.ARROW_S)
     if accelerometer.was_gesture("right"):
         # uart.write("R")
         radio.send("RR")
-#        display.show("R");
-        display.show(Image.ARROW_W)
+        # display.show("R");
+        # display.show(Image.ARROW_S)
     if accelerometer.was_gesture("left"):
         # uart.write("L")
         radio.send("RL")
-#        display.show("L");
-        display.show(Image.ARROW_E)
+        # display.show("L");
+        # display.show(Image.ARROW_S)
 
     prev = curr
     sleep(1)
